89804fef54fb6dc483c938f8fa3521d264944222
beneko <behzadnekouei@gmail.com>
2022-05-11 12:50:59 +0200

    addNewEmployee method added to EmployeeController and tested with postman

--- src/main/java/com/employees/springboot/controller/EmployeeController.java
+++ src/main/java/com/employees/springboot/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.employees.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employees.springboot.model.Employee;
 import com.employees.springboot.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -21,5 +25,13 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
+	}
+	
+	// Add new employee
+	@PostMapping("/add")
+	public String addNewEmployee(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String emailId ) {
+		Employee employee = new Employee(firstName, lastName, emailId);
+		employeeRepository.save(employee);
+		return "The new employee has been saved!";
 	}
 }
