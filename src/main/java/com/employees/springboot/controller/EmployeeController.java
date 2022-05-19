bb5e774d41f96490c22a461215edc920dabc404c
beneko <behzadnekouei@gmail.com>
2022-05-19 13:32:49 +0200

    add getEmployees method to get employees list with pagination

--- src/main/java/com/employees/springboot/controller/EmployeeController.java
+++ src/main/java/com/employees/springboot/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.employees.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employees.springboot.exception.ResourceNotFoundException;
@@ -25,9 +28,21 @@ public class EmployeeController {
 	private EmployeeRepository employeeRepository;
 	
 	// get all employees
+//	@GetMapping("/employees")
+//	public List<Employee> getAllEmployees(){
+//		return employeeRepository.findAll();
+//	}
+	
+	// find employees with pagination
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
-		return employeeRepository.findAll();
+	public List<Employee> getEmployees(@RequestParam(defaultValue = "0" ) String page){
+		Page<Employee> employees = employeeRepository.findAll(PageRequest.of(Integer.valueOf(page)-1, 5));
+		System.out.println("Total Pages : " + employees.getTotalPages());
+		System.out.println("Total Employees : " + employees.getTotalElements());
+		System.out.println("Actuel Page Number : " + employees.getNumber());
+		System.out.println("Total Pages : " + employees.get());
+		return employees.getContent();
+		
 	}
 	
 	// Add new employee
