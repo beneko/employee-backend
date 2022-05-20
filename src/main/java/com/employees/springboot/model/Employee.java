a90de9160463c7f3af91d266e8ce2321d6b47bf7
beneko <behzadnekouei@gmail.com>
2022-05-20 11:52:10 +0200

    add some fields to employee entity

--- src/main/java/com/employees/springboot/model/Employee.java
+++ src/main/java/com/employees/springboot/model/Employee.java
@@ -1,11 +1,15 @@
 package com.employees.springboot.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "employees")
@@ -24,22 +28,62 @@ public class Employee {
 	@Column(name = "email_id")
 	private String emailId;
 	
+	@Column(name= "score")
+	private int score;
+	
+	@Column(name= "arret_maladie")
+	private boolean malade;
+	
+	@Column(name= "date_embauche")
+	@Temporal(TemporalType.DATE)
+	private Date embaucheDate;
+	
 	public Employee() {
 
 	}
 	
-	public Employee(String firstName, String lastName, String emailId) {
+	public Employee(Long id, String firstName, String lastName, String emailId, int score, boolean malade,
+			Date embaucheDate) {
 		super();
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.emailId = emailId;
+		this.score = score;
+		this.malade = malade;
+		this.embaucheDate = embaucheDate;
 	}
+
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public boolean isMalade() {
+		return malade;
+	}
+
+	public void setMalade(boolean malade) {
+		this.malade = malade;
+	}
+
+	public Date getEmbaucheDate() {
+		return embaucheDate;
+	}
+
+	public void setEmbaucheDate(Date embaucheDate) {
+		this.embaucheDate = embaucheDate;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -62,6 +106,6 @@ public class Employee {
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
-				+ "]";
+				+ ", score=" + score + ", malade=" + malade + ", embaucheDate=" + embaucheDate + "]";
 	}
 }
